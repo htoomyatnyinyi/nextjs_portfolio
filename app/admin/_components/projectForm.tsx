1149354e0b0f875f05c0ca108fb481bdedee8367
htoomyatnyinyi <htoomyatnyinyi@gmail.com>
2025-07-20 16:04:43 +0700

    revise: admin-projectform

--- app/admin/_components/projectForm.tsx
+++ app/admin/_components/projectForm.tsx
@@ -1,45 +1,55 @@
-"use client";
+// "use client";
 
-import { addProject } from "../_actions/product";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
-import z from "zod";
+// import { addProject } from "../_actions/product";
+// import { Input } from "@/components/ui/input";
+// import { Button } from "@/components/ui/button";
+// import { Label } from "@/components/ui/label";
+// import z from "zod";
 
-const ProjectForm = () => {
+// const ProjectForm = () => {
+//   return (
+//     <div className="p-2 m-1 ">
+//       <h1>Project Form</h1>
+//       <form
+//         action={async (formData) => {
+//           await addProject(formData);
+//         }}
+//         className="p-2 space-y-8"
+//       >
+//         <div className="space-y-2">
+//           <Label htmlFor="title">Name</Label>
+//           <Input
+//             type="text"
+//             id="title"
+//             name="title"
+//             placeholder="Project Title"
+//             required
+//           />
+//         </div>
+//         <div className="space-y-2">
+//           <Label htmlFor="description">Description</Label>
+//           <Input
+//             type="text"
+//             id="description"
+//             name="description"
+//             placeholder="Project Description"
+//             required
+//           />
+//         </div>
+//         <Button type="submit">Save</Button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default ProjectForm;
+
+import React from 'react'
+
+const projectForm = () => {
   return (
-    <div className="p-2 m-1 ">
-      <h1>Project Form</h1>
-      <form
-        action={async (formData) => {
-          await addProject(formData);
-        }}
-        className="p-2 space-y-8"
-      >
-        <div className="space-y-2">
-          <Label htmlFor="title">Name</Label>
-          <Input
-            type="text"
-            id="title"
-            name="title"
-            placeholder="Project Title"
-            required
-          />
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="description">Description</Label>
-          <Input
-            type="text"
-            id="description"
-            name="description"
-            placeholder="Project Description"
-            required
-          />
-        </div>
-        <Button type="submit">Save</Button>
-      </form>
-    </div>
-  );
-};
+    <div>projectForm</div>
+  )
+}
 
-export default ProjectForm;
+export default projectForm
